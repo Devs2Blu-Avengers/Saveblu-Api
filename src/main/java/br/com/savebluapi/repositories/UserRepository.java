4d24261045f831b624c94f67705b4799b87eecfd
kaue <kaue.ochner@outlook.com>
2023-10-21 18:53:34 -0300

    Create UserService and UserRepository
    For UserServicre:
    Created functions:
    findById, findAll, findAllDevicesForUserId, findUserByEmail, findByName (return a List), findUserByTelephone, create, update and delete.
    For UserRepository:
    created  simple attributes witch lambok. Query for findDevicesByUserId

--- src/main/java/br/com/savebluapi/repositories/UserRepository.java
+++ src/main/java/br/com/savebluapi/repositories/UserRepository.java
@@ -1,6 +1,23 @@
 package br.com.savebluapi.repositories;
+import java.util.List;
 
-public class UserRepository {
-    // TODO: escrever a classe UserRepository  
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
+import org.springframework.stereotype.Repository;
+
+import br.com.savebluapi.models.Device;
+import br.com.savebluapi.models.User;
+
+@Repository
+public interface UserRepository extends JpaRepository<User, Integer> {
+    User findByEmail(String email);
+    User findByTelephone(String telephone);
+    boolean existsByEmail(String email);
+    boolean existsByTelephone(String telephone);
     
+    List<User> findByName(String name);
+    
+    @Query("SELECT u.devices FROM User u WHERE u.id = :userId")
+    List<Device> findDevicesByUserId(@Param("userId") int userId);
 }
