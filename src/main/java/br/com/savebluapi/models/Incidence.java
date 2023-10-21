29d692b1bb41bf1270331eb52e9e74e2056be327
NeoJhonn <jhonny.azevedo@gmail.com>
2023-10-21 17:15:59 -0300

    Model Incidence, Enum Category e IncidenceDTO criados

--- src/main/java/br/com/savebluapi/models/Incidence.java
+++ src/main/java/br/com/savebluapi/models/Incidence.java
@@ -1,5 +1,45 @@
 package br.com.savebluapi.models;
 
+import br.com.savebluapi.enums.Category;
+import jakarta.persistence.*;
+import lombok.Data;
+
+
+import java.util.Date;
+
+@Data
+@Entity
 public class Incidence {
-    // TODO: escrever a model Incidence
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Temporal(TemporalType.DATE)
+    @Column(nullable = false)
+    private Date date;
+
+    @Column(nullable = false)
+    private Double latitude;
+
+    @Column(nullable = false)
+    private Double longitude;
+
+    @Column(nullable = false)
+    private Category category;
+
+    @Column(nullable = false)
+    private String description;
+
+    @ManyToOne(optional = true, fetch = FetchType.LAZY)
+    private User user;
+
+    @Column
+    private Boolean urgent;
+
+    @Column(nullable = false)
+    private Boolean status;
+
+    @Column
+    private Boolean valid;
 }
