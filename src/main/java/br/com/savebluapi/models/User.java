9bb861652f35460f4bf665ee564e2969767979ba
kaue <kaue.ochner@outlook.com>
2023-10-21 16:31:08 -0300

    Update User
    added rules for attributes.

--- src/main/java/br/com/savebluapi/models/User.java
+++ src/main/java/br/com/savebluapi/models/User.java
@@ -3,10 +3,15 @@ package br.com.savebluapi.models;
 import java.util.List;
 
 import br.com.savebluapi.enums.UserType;
+import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
+import jakarta.persistence.EnumType;
+import jakarta.persistence.Enumerated;
+import jakarta.persistence.FetchType;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
+import jakarta.persistence.OneToMany;
 import lombok.Data;
 
 @Data
@@ -17,10 +22,21 @@ public class User {
 	@GeneratedValue(strategy = GenerationType.IDENTITY)
     int id;
     
+	@Column(nullable = false, unique = true)
 	private String email;
+	
+	@Column(nullable = false)
 	private String name;
+	
+	@Column(nullable = false, unique = true)
 	private String telephone;
+	
+	@Enumerated(EnumType.STRING)
+	@Column(nullable = false)
 	private UserType type;
+	
+	@Column(nullable = false)
+	@OneToMany(mappedBy = "device", fetch = FetchType.LAZY)
 	private List<Device> devices;
 	
 }
