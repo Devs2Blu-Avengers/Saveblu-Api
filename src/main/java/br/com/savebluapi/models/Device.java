5b3995c999bb52bda4793aaf003383fbd4fb4a68
Rodrigo Alejandro Mora Azocar <brazuk25@gmail.com>
2023-10-21 16:39:33 -0300

    Model Device

--- src/main/java/br/com/savebluapi/models/Device.java
+++ src/main/java/br/com/savebluapi/models/Device.java
@@ -1,23 +1,31 @@
 package br.com.savebluapi.models;
 
+import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
 import jakarta.persistence.FetchType;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
 import jakarta.persistence.ManyToOne;
 import lombok.Data;
-
 
 @Data
 @Entity
 public class Device {
-    // TODO: escrever a model Device
-	
+
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private Long id;
+	@Column(nullable = false)
 	private String token;
-	@ManyToOne(optional = true, fetch = FetchType.LAZY)
+	
+	@ManyToOne(optional = false, fetch = FetchType.LAZY)
 	private User user;
-	
+
+	@Column(nullable = false)
 	private double last_latitude;
-	private double last_longitude;
-	private String number;
 	
-    
+	@Column(nullable = false)
+	private double last_longitude;
+	private String number; 
 }
