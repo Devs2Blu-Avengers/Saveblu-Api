e7d6c3833d6dba434dcaabb48133936dda03e1cf
Regis Amaral <amaral.regis@gmail.com>
2023-10-22 01:22:55 -0300

    Altera DeviceService

--- src/main/java/br/com/savebluapi/services/DeviceService.java
+++ src/main/java/br/com/savebluapi/services/DeviceService.java
@@ -1,5 +1,19 @@
 package br.com.savebluapi.services;
 
+import org.modelmapper.ModelMapper;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import br.com.savebluapi.repositories.DeviceRepository;
+
+@Service
 public class DeviceService {
-    // TODO: escrever a service DeviceService
+    
+    @Autowired
+    DeviceRepository repository;
+
+    @Autowired
+    ModelMapper mapper;
+
+    
 }
